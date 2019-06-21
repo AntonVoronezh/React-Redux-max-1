017f8647ab0f65078c216da69c8454d2b2e10e44
AntonVoronezh <pstut@mail.ru>
2019-06-21 17:19:59 +0300

    add TopMenu

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,21 @@
 import React, { Fragment } from 'react';
 import './App.scss';
 import { LoginPage, IndexPage, NewsPage, ProfilePage } from '../pages';
+import { TopMenu } from '../elements';
+import { Container } from 'semantic-ui-react';
 
 const App = () => {
 	return (
-		<Fragment>
-			<IndexPage />
-			<NewsPage />
-			<ProfilePage />
-			<LoginPage />
-		</Fragment>
+		<Container>
+			<Fragment>
+
+				<TopMenu />
+				<IndexPage />
+				<NewsPage />
+				<ProfilePage />
+				<LoginPage />
+			</Fragment>
+		</Container>
 	);
 };
 
---
+++ src/components/elements/TopMenu/TopMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TopMenu.scss';
+import { Menu } from 'semantic-ui-react';
+
+const TopMenu = () => {
+	return (
+		<Menu>
+			<h2>TopMenu</h2>
+		</Menu>
+	);
+};
+
+export default TopMenu;
