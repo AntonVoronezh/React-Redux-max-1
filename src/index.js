b0a1e83f09d8995ad47a73b6fe6c6749b20c168b
AntonVoronezh <pstut@mail.ru>
2019-06-21 16:44:34 +0300

    add App:'

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import createStore from './store/store';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import createStore from './store/store';
 import App from './containers/App/App';
 import 'semantic-ui-css/semantic.min.css';
 
-const store = createStore();
+// const store = createStore();
 
 ReactDOM.render(
-	<Provider store={store}>
-		<BrowserRouter>
+	// <Provider store={store}>
+		// <BrowserRouter>
 			<App />
-		</BrowserRouter>
-	</Provider>,
+		// </BrowserRouter>
+	// </Provider>
+	,
 	document.getElementById('root')
 );
