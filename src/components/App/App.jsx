91f86d186a446e98c06a8f4a615d202bf0e0b728
AntonVoronezh <pstut@mail.ru>
2019-06-21 17:36:43 +0300

    add Route in App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,18 +3,19 @@ import './App.scss';
 import { LoginPage, IndexPage, NewsPage, ProfilePage } from '../pages';
 import { TopMenu } from '../elements';
 import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
 
 const App = () => {
 	return (
 		<Container className="top">
-			<Fragment>
+			{/* <Fragment> */}
+			<TopMenu />
+			<Route path="/" render={() => <IndexPage />} />
+			<Route path="/news" render={() => <NewsPage />} />
+			<Route path="/profile" render={() => <ProfilePage />} />
+			<Route path="/login" render={() => <LoginPage />} />
 
-				<TopMenu />
-				<IndexPage />
-				<NewsPage />
-				<ProfilePage />
-				<LoginPage />
-			</Fragment>
+			{/* </Fragment> */}
 		</Container>
 	);
 };
