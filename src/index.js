26a62ce3c4a5a6391b70c7dab9ed44e045ee411b
AntonVoronezh <pstut@mail.ru>
2019-06-21 17:31:12 +0300

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 // import createStore from './store/store';
 import App from './components/App';
@@ -10,10 +10,9 @@ import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
 	// <Provider store={store}>
-		// <BrowserRouter>
-			<App />
-		// </BrowserRouter>
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
 	// </Provider>
-	,
 	document.getElementById('root')
 );
