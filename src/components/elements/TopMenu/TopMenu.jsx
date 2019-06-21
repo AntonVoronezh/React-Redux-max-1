3292c84beb63c6b867e8e55d0190dcd0473c0e5e
AntonVoronezh <pstut@mail.ru>
2019-06-21 17:33:11 +0300

    add Link to TopMenu

--- src/components/elements/TopMenu/TopMenu.jsx
+++ src/components/elements/TopMenu/TopMenu.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './TopMenu.scss';
 import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const TopMenu = () => {
 	return (
 		<Menu>
-			<Menu.Item content="На главную" active={true} />
-			<Menu.Item content="Новости" />
-			<Menu.Item content="Профайл" />
-			<Menu.Item content="Логин" position="right" />
+			<Link to="/">
+				<Menu.Item content="На главную" active={true} />
+			</Link>
+			<Link to="/news">
+				<Menu.Item content="Новости" />
+			</Link>
+			<Link to="/profile">
+				<Menu.Item content="Профайл" />
+			</Link>
+			<Link to="/login">
+				<Menu.Item content="Логин" position="right" />
+			</Link>
 		</Menu>
 	);
 };
