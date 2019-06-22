7324a034b17efd7f454d46e442892005a9f7340b
AntonVoronezh <pstut@mail.ru>
2019-06-22 14:44:40 +0300

    add login action

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,4 @@
 import { changeActivePageAC, CHANGE_ACTIVE_PAGE } from './menu';
+import { CHANGE_USERNAME_TEXT, CHANGE_PASSWORD_TEXT } from './menu';
 
-export { changeActivePageAC, CHANGE_ACTIVE_PAGE };
+export { changeActivePageAC, CHANGE_ACTIVE_PAGE, CHANGE_USERNAME_TEXT, CHANGE_PASSWORD_TEXT };
---
+++ src/store/actions/login.js
@@ -0,0 +1,13 @@
+const CHANGE_USERNAME_TEXT = 'CHANGE_USERNAME_TEXT';
+const CHANGE_PASSWORD_TEXT = 'CHANGE_PASSWORD_TEXT';
+
+const changeUsernameTextAC = text => ({
+	type: CHANGE_USERNAME_TEXT,
+	text,
+});
+const changePasswordTextAC = text => ({
+	type: CHANGE_PASSWORD_TEXT,
+	text,
+});
+
+export { changeUsernameTextAC, CHANGE_USERNAME_TEXT, changePasswordTextAC, CHANGE_PASSWORD_TEXT };
