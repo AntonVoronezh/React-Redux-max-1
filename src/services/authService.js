cfd1cad36a1878200c22f492d1d0fbf74296737c
AntonVoronezh <pstut@mail.ru>
2019-06-23 15:10:04 +0300

    ref authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,30 +1,40 @@
+export default class AuthService {
 
-export default class BookstoreService {
+	tryLogin(username, password) {
+		console.log('AuthService -tryLogin ', username, password);
+	}
 
-  data = [
-    {
-      id: 1,
-      title: 'Production-Ready Microservices',
-      author: 'Susan J. Fowler',
-      price: 32,
-      coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg'},
-    {
-      id: 2,
-      title: 'Release It!',
-      author: 'Michael T. Nygard',
-      price: 45,
-      coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg'}
-  ];
+	// username: Admin
+	// password: 12345
+	// ```
+	// Если введены другие данные, то показывается сообщения:
 
-  getBooks() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (Math.random() > 0.75) {
-          reject(new Error('Something bad happened'));
-        } else {
-          resolve(this.data);
-        }
-      }, 700);
-    });
-  }
+	// Имя пользователя или пароль введены не верно
+
+	// data = [
+	//   {
+	//     id: 1,
+	//     title: 'Production-Ready Microservices',
+	//     author: 'Susan J. Fowler',
+	//     price: 32,
+	//     coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg'},
+	//   {
+	//     id: 2,
+	//     title: 'Release It!',
+	//     author: 'Michael T. Nygard',
+	//     price: 45,
+	//     coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg'}
+	// ];
+
+	// getBooks() {
+	//   return new Promise((resolve, reject) => {
+	//     setTimeout(() => {
+	//       if (Math.random() > 0.75) {
+	//         reject(new Error('Something bad happened'));
+	//       } else {
+	//         resolve(this.data);
+	//       }
+	//     }, 700);
+	//   });
+	// }
 }
