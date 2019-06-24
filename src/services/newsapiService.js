f5f6897f18657a33a71df8b9f975deb064e94412
AntonVoronezh <pstut@mail.ru>
2019-06-24 15:41:05 +0300

    add getResourse

--- src/services/newsapiService.js
+++ src/services/newsapiService.js
@@ -6,3 +6,19 @@
 // export default () => {
 //    return axios.get(`${url}top-headlines?sources=bbc-news&apiKey=${apiKey}`)
 // }
+
+export default class newsapiServises {
+	_baseUrl = 'https://newsapi.org/v2/';
+    _apiKey = '7fd45e35dc9349c8ad721a77101089d6';
+    
+    getResourse = async url => {
+		const response = await fetch(`${this._baseUrl}top-headlines?sources=bbc-news&apiKey=${this._apiKey}`);
+
+		if (!response.ok) {
+			throw new Error(`Could not fetch ${url}, resived ${response.status}`);
+		}
+		const body = await response.json();
+
+		return body;
+	};
+}
